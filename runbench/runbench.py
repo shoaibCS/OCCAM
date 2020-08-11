2cfa66807fd4cb2c579683ac20a8c8c921f4699e
shoaibCS <19100080@lums.edu.pk>
2020-08-11 20:49:18 +0000

    Adding trimmer.set for the trimmer applications

--- runbench/runbench.py
+++ runbench/runbench.py
@@ -6,24 +6,24 @@
 
 # NOTE: this script reads from the standard output/error to extract
 # information about OCCAM and ROPgadget. Thus, any change in these
-# tools might break this script. 
+# tools might break this script.
 
 import sys
 import os
 import os.path
 import re
-import datetime 
+import datetime
 import collections
 import argparse as a
 import pptable
 import commands as cmd
 
 verbose = False
-# named tuple for occam stats
+# named tuple for occam stats 'portfolio', 'spec2006'
 occam_stats = collections.namedtuple('OccamStats', 'funcs insts mem_insts')
 
 def get_benchmark_sets():
-    return ['portfolio', 'spec2006']
+    return ['portfolio', 'spec2006', 'trimmer']
 
 def get_occam_home():
     path = os.environ.get('OCCAM_HOME')
@@ -35,11 +35,11 @@ def get_ropgadget():
     ropgadget = None
     if 'ROPGADGET' in os.environ: ROPGADGET = os.environ ['ROPGADGET']
     if not cmd.is_exec(ropgadget): ropgadget = cmd.which('ropgadget')
-    if not cmd.is_exec(ropgadget): ropgadget = cmd.which('ROPgadget.py')    
+    if not cmd.is_exec(ropgadget): ropgadget = cmd.which('ROPgadget.py')
     if not cmd.is_exec(ropgadget):
         raise IOError("Cannot find ropgadget")
     return ropgadget
-    
+
 def get_benchmarks(name):
     import json
     with open(name + ".set", "r") as f:
@@ -56,7 +56,7 @@ def get_benchmarks(name):
 def read_occam_output(logfile):
     b_funcs, b_insts, b_mem = 0, 0, 0
     a_funcs, a_insts, a_mem = 0, 0, 0
-    before_done = False    
+    before_done = False
     with open(logfile, 'r') as fd:
         for line in fd:
             if re.search('^Statistics for \S* after specialization', line):
@@ -101,7 +101,7 @@ def pretty_printing_occam(results):
     for benchmark, before, after in results:
         if before is None or after is None:
             continue
-        
+
         func_red = 0.0
         if (before.funcs > 0):
             func_red = (1.0 - (float(after.funcs) / float(before.funcs))) * 100.0
@@ -116,20 +116,20 @@ def pretty_printing_occam(results):
                     after.funcs,
                     float("{0:.2f}".format(func_red)),
                     before.insts,
-                    after.insts,                    
+                    after.insts,
                     float("{0:.2f}".format(insts_red)),
                     before.mem_insts,
-                    after.mem_insts,                    
+                    after.mem_insts,
                     float("{0:.2f}".format(mem_red))])
 
     table = [["Program", \
               "B Fun", "A Fun", "% Fun Red", \
               "B Ins", "A Ins", "% Ins Red", \
               "B Mem Ins", "A Mem Ins", "% Mem Ins Red"]]
-    
+
     for row in tab:
         table.append(row)
-        
+
     import pptable
     out = sys.stdout
     pptable.pprint_table(out,table)
@@ -141,7 +141,7 @@ def pretty_printing_ropgadget(results):
         jop_before, jop_after, \
         sys_before, sys_after, \
         rop_before, rop_after in results:
-    
+
         # total_red = 0.0
         # if (total_before > 0):
         #     total_red = (1.0 - (float(total_after) / float(total_before))) * 100.0
@@ -150,11 +150,11 @@ def pretty_printing_ropgadget(results):
             rop_red = (1.0 - (float(rop_after) / float(rop_before))) * 100.0
         sys_red = 0.0
         if (sys_before > 0):
-            sys_red = (1.0 - (float(sys_after) / float(sys_before))) * 100.0        
+            sys_red = (1.0 - (float(sys_after) / float(sys_before))) * 100.0
         jop_red = 0.0
         if (jop_before > 0):
             jop_red = (1.0 - (float(jop_after) / float(jop_before))) * 100.0
-            
+
         tab.append([benchmark,
                     #total_before,
                     #total_after,
@@ -173,14 +173,14 @@ def pretty_printing_ropgadget(results):
               "B ROP", "A ROP", "% ROP Red", \
               "B SYS", "A SYS", "% SYS Red", \
               "B JOP", "A JOP", "% JOP Red"]]
-    
+
     for row in tab:
         table.append(row)
-        
+
     out = sys.stdout
     pptable.pprint_table(out,table)
-    
-    
+
+
 def run_occam(dirname, execname, workdir, cpu, mem, slash_opts= []):
     #benchmark_name = os.path.basename(os.path.normpath(dirname))
     benchmark_name = execname
@@ -204,7 +204,7 @@ def run_occam(dirname, execname, workdir, cpu, mem, slash_opts= []):
             cmd.warning("something failed while running \"" + ' '.join(slash_args) + \
                         "\"" + benchmark_name + "\n" + \
                         "Read logs " + outfile + " and " + errfile)
-        
+
     outfd.close()
     errfd.close()
 
@@ -212,11 +212,11 @@ def run_occam(dirname, execname, workdir, cpu, mem, slash_opts= []):
         #All OCCAM output is redirected to error
         logfile = os.path.join(workdir, errfile)
         (res_before, res_after) = read_occam_output(logfile)
-        
+
     return (benchmark_name, res_before, res_after)
 
-#Pre: run_occam has been executed already and thus, there are two 
-#executables in dirname: the original one and the one after occam.        
+#Pre: run_occam has been executed already and thus, there are two
+#executables in dirname: the original one and the one after occam.
 def run_ropgadget(dirname, execname, workdir, cpu, mem):
     def run_ropgadget_on_pair(bench_name, prog_before, prog_after, opts, \
                               logfile, outfd, errfd):
@@ -234,12 +234,12 @@ def run_ropgadget(dirname, execname, workdir, cpu, mem):
             errfd.seek(0,0)
             args = [get_ropgadget(), '--binary', prog_after] + opts
             returncode,_,_,_ = cmd.run_limited_cmd(args, outfd, errfd, bench_name)
-            # ROPgadget returns 1 if success        
+            # ROPgadget returns 1 if success
             if returncode <> 1:
                 cmd.warning("something failed while running \"" + ' '.join(args) + "\"")
             else:
                 res_after = read_ropgadget_output(logfile)
-                
+
         return (res_before, res_after)
 
     #benchname = os.path.basename(os.path.normpath(dirname))
@@ -253,15 +253,15 @@ def run_ropgadget(dirname, execname, workdir, cpu, mem):
     prog_after = os.path.join(dirname, benchname + "_slashed")
 
     total_before, total_after = None, None
-    rop_before, rop_after = None, None    
+    rop_before, rop_after = None, None
     jop_before, jop_after = None, None
     sys_before, sys_after = None, None
-    
-    if os.path.exists(prog_before) and os.path.exists(prog_after): 
+
+    if os.path.exists(prog_before) and os.path.exists(prog_after):
         # total gadgets: ropgadget --binary prog
         # only jop:      ropgadget --binary --norop --nosys
         # only sys:      ropgadget --binary --nojop --norop
-        # only rop:      ropgadget --binary --nojop --nosys    
+        # only rop:      ropgadget --binary --nojop --nosys
         logfile = os.path.join(workdir, outfile)
         opts = []
         (total_before, total_after) = run_ropgadget_on_pair(benchname, prog_before, prog_after, \
@@ -280,9 +280,9 @@ def run_ropgadget(dirname, execname, workdir, cpu, mem):
             cmd.warning(prog_before + " does not exist")
         if not os.path.exists(prog_after):
             cmd.warning(prog_after + " does not exist")
-            
+
     outfd.close()
-    errfd.close()    
+    errfd.close()
     return (benchname, \
             total_before, total_after, \
             jop_before, jop_after, \
@@ -312,7 +312,7 @@ def parse_opt (argv):
     p.add_argument ('--ipdse',
                     help="Enable inter-procedural dead store elimination",
                     dest='use_ipdse', default=False, action="store_true")
-    
+
     args = p.parse_args (argv)
     return args
 
@@ -330,20 +330,20 @@ def main (argv):
         occam_opts += ['--ai-dce']
     if args.use_ipdse:
         occam_opts += ['--ipdse']
-    
-    dt = datetime.datetime.now ().strftime ('%d/%m/%Y %H:%M:%S')    
-    print "[" + dt + "] " +  "STARTED runbench"    
+
+    dt = datetime.datetime.now ().strftime ('%d/%m/%Y %H:%M:%S')
+    print "[" + dt + "] " +  "STARTED runbench"
     for s in sets:
         for t in get_benchmarks(s):
             if t['enabled'] == 'false':
                 continue
-            
+
             dirname = t['dirname']
             if not os.path.isdir(dirname):
                 dirname = os.path.join(get_occam_home(), dirname)
                 if not os.path.isdir(dirname):
                     cmd.raise_error(t['dirname'] + " is not a directory")
-                    
+
             execname = t['execname']
             res = run_occam(dirname, execname, workdir, args.cpu, args.mem, occam_opts)
             occam_tab.append(res)
@@ -351,20 +351,20 @@ def main (argv):
                 res = run_ropgadget(dirname, execname, workdir, args.cpu, args.mem)
                 ropgadget_tab.append(res)
 
-    dt = datetime.datetime.now ().strftime ('%d/%m/%Y %H:%M:%S')                
+    dt = datetime.datetime.now ().strftime ('%d/%m/%Y %H:%M:%S')
     print "[" + dt + "] " +  "FINISHED runbench\n"
 
     print "\nProgram Reduction: (B:before and A:after OCCAM with " + \
         ' '.join(occam_opts) + ")\n"
     pretty_printing_occam(occam_tab)
-    
+
     if args.rop:
         print "\nGadget Reduction: (B:before and A:after OCCAM with " + \
             ' '.join(occam_opts) + ")\n"
         pretty_printing_ropgadget(ropgadget_tab)
-        
+
     return 0
-    
+
 if __name__ == '__main__':
     res = None
     try:
